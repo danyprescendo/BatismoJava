d006474576470b763f475b33e1ce09afc77d5740
danyprescendo <danypablo08@hotmail.com>
2026-05-18 20:23:27 -0300

    construtores

---
+++ src/NiveIntermediario/Construtores/Hokages.java
@@ -0,0 +1,47 @@
+package NiveIntermediario.Construtores;
+
+public class Hokages {
+
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+    String aldeia;
+    int missoes;
+    double saldoBancario;
+    double altura;
+
+    public Hokages() {
+        // Construtor vazio, sem argumentos
+    }
+
+    // Criar um construtos com argumentos
+    public Hokages(String nome) {
+        this.nome = nome;
+    }
+
+    public Hokages(int idade) {
+        this.idade = idade;
+    }
+
+    // All args constructor = Construtor com todos os argumentos
+    public Hokages(String nome, int idade, boolean vivoOuNao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+    }
+
+    public Hokages(double altura) {
+        this.altura = altura;
+    }
+
+    // Criar automaticamente os construtores
+    public Hokages(String nome, int idade, boolean vivoOuNao, String aldeia, int missoes, double saldoBancario, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+        this.aldeia = aldeia;
+        this.missoes = missoes;
+        this.saldoBancario = saldoBancario;
+        this.altura = altura;
+    }
+}
