1bd06811d11cc799fc3f57fa7b4079dc464ba5bd
danyprescendo <danypablo08@hotmail.com>
2026-06-04 21:21:13 -0300

    getters and setters

---
+++ src/NiveIntermediario/Encapsulamento/Ninja.java
@@ -0,0 +1,68 @@
+package NiveIntermediario.Encapsulamento;
+
+public abstract class Ninja {
+
+    private String nome;
+    private String aldeia;
+    int idade;
+    int numeroDeMissoesConcluidas;
+    double altura = 2.10;
+
+    public Ninja() {
+    }
+
+    public Ninja(String nome, String aldeia, int idade, int numeroDeMissoesConcluidas, double altura) {
+        this.nome = nome;
+        this.aldeia = aldeia;
+        this.idade = idade;
+        this.numeroDeMissoesConcluidas = numeroDeMissoesConcluidas;
+        this.altura = altura;
+    }
+
+    // Tirar dados ou mostrar pro usuário eu uso o GET + NOME DA VARIAVEL
+    // GETTER não recebe valores, apenas retorna
+
+    // Inlcuir dados na varíavel eu uso o SET + NOME DA VARIAVEL
+    // SETTER recebe o valor a ser definido na variável
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAldeia() {
+        return aldeia;
+    }
+
+    public void setAldeia(String aldeia) {
+        this.aldeia = aldeia;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getNumeroDeMissoesConcluidas() {
+        return numeroDeMissoesConcluidas;
+    }
+
+    public void setNumeroDeMissoesConcluidas(int numeroDeMissoesConcluidas) {
+        this.numeroDeMissoesConcluidas = numeroDeMissoesConcluidas;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
