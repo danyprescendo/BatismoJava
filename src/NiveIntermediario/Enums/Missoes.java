d9ca398d815eb141e439fd8d781a65a580041290
danyprescendo <danypablo08@hotmail.com>
2026-06-04 21:40:44 -0300

    enums

---
+++ src/NiveIntermediario/Enums/Missoes.java
@@ -0,0 +1,36 @@
+package NiveIntermediario.Enums;
+
+public class Missoes {
+
+    private String nome;
+    private RankDeMissoes rank;
+
+    // Metodo para mostrar mais informações
+    public void exibirDetalhes() {
+        System.out.println("Missão: " + nome + " - Rank: " + rank + " (Descrição: " + rank.getDescricao() + ", Dificuldade: " + rank.getDificuldade() + ")");
+    }
+
+    public Missoes() {
+    }
+
+    public Missoes(String nome, RankDeMissoes rank) {
+        this.nome = nome;
+        this.rank = rank;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public RankDeMissoes getRank() {
+        return rank;
+    }
+
+    public void setRank(RankDeMissoes rank) {
+        this.rank = rank;
+    }
+}
---
+++ src/NiveIntermediario/Enums/RankDeMissoes.java
@@ -0,0 +1,34 @@
+package NiveIntermediario.Enums;
+
+public enum RankDeMissoes {
+
+    D("Baixo", 2),
+    C("Moderado", 3),
+    B("Confortável", 4),
+    A("Difícil", 5),
+    S("Altíssimo", 12);
+
+    private String descricao;
+    private int dificuldade;
+
+    RankDeMissoes(String descricao, int dificuldade) {
+        this.descricao = descricao;
+        this.dificuldade = dificuldade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(int dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+}
