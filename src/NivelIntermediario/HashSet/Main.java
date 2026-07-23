09fb9b5a5055c2f960614eb644164b49525323ef
danyprescendo <danypablo08@hotmail.com>
2026-07-23 20:46:23 -0300

    hashset

---
+++ src/NivelIntermediario/HashSet/Main.java
@@ -0,0 +1,25 @@
+package NivelIntermediario.HashSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> ninjas = new ArrayList<>();
+        ninjas.add("Naruto Uzumaki");
+        ninjas.add("Sasuke Uchiha");
+        ninjas.add("Sakura Haruno");
+        ninjas.add("Naruto Uzumaki");
+
+        System.out.println(ninjas);
+
+        Set<String> ninjasSet = new HashSet<>();
+        ninjasSet.addAll(ninjas);
+
+        System.out.println(ninjasSet);
+
+    }
+}
